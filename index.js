86c0914d2387bffa11ab0fcfdbb090efa73b9e88
Trần Việt Anh <95759256+tranvietanh20112001@users.noreply.github.com>
2024-06-04 15:58:13 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("facebook").addEventListener("click", function () {
+  window.open("https://www.facebook.com/loyd.parker.5688/", "_blank");
+});
+
+document.getElementById("instagram").addEventListener("click", function () {
+  window.open("https://www.instagram.com/_tvanh2011_/", "_blank");
+});
